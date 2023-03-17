1801f990d3d556e38c7efc067247153e5af1d2ee
Charley <charley1135@gmail.com>
2023-03-16 17:42:00 -0700

    Feat: Added in delete button.

--- Books/books/src/App.js
+++ Books/books/src/App.js
@@ -6,14 +6,24 @@ import BookList from "./components/BookList";
 const App = () => {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+        return book.id !== id;
+    })
+    setBooks(updatedBooks);
+  };
+
   const handleCreateBook = (title) => {
     const updatedBooks = [...books, { id: Math.round(Math.random()) * 9999, title: title }];
 
     setBooks(updatedBooks);
   };
+
+  
+
   return (
     <div className="app">
-      <BookList books={books}/>
+      <BookList books={books} onDelete={deleteBookById}/>
       <BookCreate onSubmit={handleCreateBook} />
     </div>
   );
