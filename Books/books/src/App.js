1bff381b7018193be961a961a9c357df0ef80190
Charley <charley1135@gmail.com>
2023-03-16 17:30:54 -0700

    Feat: Book show + Book list component's created.

--- Books/books/src/App.js
+++ Books/books/src/App.js
@@ -1,10 +1,22 @@
-const App = () => {
-    return ( 
+import React, { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
-        <div>
-            App
-        </div>
-     );
-}
- 
+
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  const handleCreateBook = (title) => {
+    const updatedBooks = [...books, { id: Math.round(Math.random()) * 9999, title: title }];
+
+    setBooks(updatedBooks);
+  };
+  return (
+    <div className="app">
+      <BookList books={books}/>
+      <BookCreate onSubmit={handleCreateBook} />
+    </div>
+  );
+};
+
 export default App;
