c2a07d29bf27c1d2a312c791319f68cf4c1cbb90
Charley <charley1135@gmail.com>
2023-03-27 15:12:40 -0700

    Feat: Created new project Comps + created App + Button components with prop types to validate props.

---
+++ Comps/comps/src/App.js
@@ -0,0 +1,13 @@
+import Button from './Button'
+
+function App() {
+    return <div>
+        <div><Button success rounded outline>Click!</Button></div>
+        <div><Button danger outline>Buy now!</Button></div>
+        <div><Button warning outline>See deal!</Button></div>
+        <div><Button>Hide Adds!</Button></div>
+        <div><Button>Jello</Button></div>
+    </div>;
+}
+
+export default App;
