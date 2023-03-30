74630218f68635ab750aa4d9acc6f1e3feb65bcb
Charley <charley1135@gmail.com>
2023-03-30 09:06:24 -0700

    Feat: added conditional rendering + tailwind styles to accordian component.

--- Comps/comps/src/components/Accordion.js
+++ Comps/comps/src/components/Accordion.js
@@ -1,14 +1,50 @@
+import React, { useState } from "react";
+import { GoChevronLeft, GoChevronDown } from "react-icons/go";
+
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (nextIndex) => {
+    //function version - only use if new value depends on old.
+    // setExpandedIndex((currentExpandedIndex) => {
+    //     if (currentExpandedIndex === nextIndex) {
+    //         return -1;
+    //     } else {
+    //         return nextIndex;
+    //     }
+    // })
+    if (expandedIndex === nextIndex) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(nextIndex);
+    }
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const icon = (
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    );
+
     return (
-    <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
-    </div>
+      <div key={item.id}>
+        {/* <div onClick={() => setExpandedIndex(index)}>{item.label}</div> */}
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
+      </div>
     );
   });
-  
-    return <div>{renderedItems}</div>;
+
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
