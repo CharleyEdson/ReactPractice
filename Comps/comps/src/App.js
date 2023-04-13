254299a37552006e8eca54c4cb5bee2152f473b4
Charley <charley1135@gmail.com>
2023-04-13 09:55:14 -0700

    Feat: Added navigation context.

--- Comps/comps/src/App.js
+++ Comps/comps/src/App.js
@@ -2,24 +2,7 @@ import Dropdown from "./components/Dropdown";
 import React, { useState } from "react";
 
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
-  return (
-    <div className="flex">
-    <Dropdown value={selection} onChange={handleSelect} options={options} />
-    <Dropdown value={selection} onChange={handleSelect} options={options} />
-    </div>
-  );
+ <div></div>
 }
 
 export default App;
