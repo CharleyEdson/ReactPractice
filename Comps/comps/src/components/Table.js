757b0cc073195ec3b076b952d3f17b66cde998bf
Charley <charley1135@gmail.com>
2023-04-18 10:13:32 -0700

    feat: Added in sortable table component.

--- Comps/comps/src/components/Table.js
+++ Comps/comps/src/components/Table.js
@@ -1,12 +1,21 @@
+import { Fragment } from "react";
+
 function Table({ data, config, keyFn }) {
   const renderedHeaders = config.map((column) => {
+    if (column.header) {
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
+    }
     return <th key={column.label}>{column.label}</th>;
   });
 
   const renderedRows = data.map((rowData) => {
     const renderedCells = config.map((column) => {
-        return <td className="p-3" key={column.label}>{column.render(rowData)}</td>
-    })
+      return (
+        <td className="p-3" key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
+    });
     return (
       <tr className="border-b" key={keyFn(rowData)}>
         {renderedCells}
